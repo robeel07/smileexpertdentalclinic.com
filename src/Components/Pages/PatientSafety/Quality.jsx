6e96d48bdaff5e78c7e351d072075a471b53ea6b
robeel07 <manuch265@gmail.com>
2026-01-07 20:35:10 +0500

    quality page

---
+++ src/Components/Pages/PatientSafety/Quality.jsx
@@ -0,0 +1,322 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import quality from "../../Images/quality.jpg";
+import icon1 from "../../Images/icon1.jpg";
+import icon2 from "../../Images/icon2.jpg";
+import icon3 from "../../Images/icon3.jpg";
+import icon4 from "../../Images/icon4.jpg";
+import icon5 from "../../Images/icon5.jpg";
+import icon6 from "../../Images/icon6.jpg";
+import icon7 from "../../Images/icon7.jpg";
+import icon8 from "../../Images/icon8.jpg";
+import icon9 from "../../Images/icon9.jpg";
+import icon10 from "../../Images/icon10.jpg";
+import icon11 from "../../Images/icon11.jpg";
+import icon12 from "../../Images/icon12.jpg";
+import icon13 from "../../Images/icon13.jpg";
+import icon14 from "../../Images/icon14.jpg";
+import icon15 from "../../Images/icon15.jpg";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+
+export default function Quality() {
+  return (
+    <>
+    <div
+      className="relative w-full h-[45vh] bg-cover bg-[center_70%]"
+      style={{ backgroundImage: `url(${quality})` }}
+    >
+      <div className="absolute inset-0 bg-black/50"></div>
+
+    <div className="relative h-full max-w-6xl px-4 flex items-end">
+  <div className="pb-8 mt-6 w-full">
+    <h2 className="text-white text-xl ml-40 mb-2">Patient Safety</h2>
+    <h1 className="text-white text-3xl md:text-5xl ml-40 font-bold">
+      Quality
+    </h1>
+  </div>
+</div>
+    </div>
+
+    {<Link to="/PatientSafety/quality"></Link>}
+
+    <div className="w-full bg-[#f3f8fb] py-6 px-4">
+  <div className="max-w-7xl mx-auto flex items-center justify-center gap-10 text-center">
+    
+    <span className="text-[#0a1f7a] font-medium text-2xl">
+      Efficient and Safe Dental Care
+    </span>
+
+    <span className="text-[#39c0c7] text-5xl font-extrabold">→</span>
+
+    <span className="text-[#0a1f7a] font-medium text-2xl">
+      Standardization of Dental Services
+    </span>
+
+    <span className="text-[#39c0c7] text-5xl font-extrabold">→</span>
+
+    <span className="text-[#0a1f7a] font-medium text-2xl">
+      Upgradation of Technical Skills
+    </span>
+
+  </div>
+</div>
+
+ <div className="max-w-7xl mx-auto px-6 py-12">
+  <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+
+    {/* LEFT Side */}
+    <div className="bg-[#f3f7fb] rounded-3xl p-6">
+      {/* 9 Images */}
+      <div className="grid grid-cols-3 gap-3 mb-6">
+        {[icon7, icon8, icon9, icon10, icon11, icon12, icon13, icon14, icon15].map((img, i) => (
+          <div
+            key={i}
+            className="w-full h-20 rounded-xl overflow-hidden"
+          >
+            <img
+              src={img}
+              alt="patient safety"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+
+      <h2 className="text-2xl font-semibold px-9 text-[#0a1f8f] mb-6">
+        Patient Safety
+      </h2>
+
+      <ul className="space-y-1 text-[#0a1f8f] font-medium">
+  {[
+    "7X SAFETY",
+    "4 Step Sterilization",
+    "Safety Equipment",
+    "Equipment & Technology",
+    "Quality",
+  ].map((item, i) => (
+    <li
+      key={i}
+      className="group flex items-center gap-3 cursor-pointer
+                 px-0 py-2 rounded-lg
+                 transition-all duration-300
+                 hover:bg-white hover:shadow-sm"
+    >
+      {/* Left indicator */}
+      <span
+        className="w-1 h-0 bg-[#0aa6ff] rounded-full
+                   transition-all duration-300
+                   group-hover:h-6"
+      ></span>
+
+      {/* Text */}
+      <span
+        className="transition-colors duration-300
+                   group-hover:text-[#0aa6ff]"
+      >
+        {item}
+      </span>
+    </li>
+  ))}
+</ul>
+
+    </div>
+
+    {/* RIGHT SIDE */}
+    <div className="lg:col-span-2 space-y-1">
+
+      {/* STEP 1 */}
+      <div className="flex gap-6 items-start ">
+        <img
+          src={icon1}
+          alt="icon"
+          className="w-32 h-40 rounded-xl object-cover mb-4"
+        />
+        <div>        
+          <p className="text-gray-600 leading-relaxed mt-4">
+            Robust policies & protocols which are standardized are mandated for the clinics. This ensures ensure 100% sterile clinic for staff, patients & attendants. These policies & protocols have been enhanced 7X to formulate the New Safety Protocols to make our clinic safest in the world.
+          </p>
+        </div>
+      </div>
+
+      {/* STEP 2 */}
+      <div className="flex gap-6 items-start">
+        <img
+          src={icon2}
+          alt="icon"
+          className="w-32 h-40 rounded-2xl object-cover mb-4"
+        />
+        <div>
+          <p className="text-gray-600 leading-relaxed mt-4 ">
+            Bi-monthly surprise Quality Audits & Mock Drills in clinic to ensure complete implementation of clinical SOPs by Doctors and Dental Assistants. This includes review of adequacy of PPE to ensure all clinics follow set protocols.
+          </p>
+        </div>
+      </div>
+
+      {/* STEP 3 */}
+      <div className="flex gap-6 items-start">
+        <img
+          src={icon3}
+          alt="icon"
+          className="w-32 h-40 rounded-2xl object-cover"
+        />
+        <div>
+          <p className="text-gray-600 leading-relaxed mt-4">
+            Patient records are digitized and stored in a highly secured cloud storage. The records can be accessed only by SmileExpert Doctors. This Patient Record Software along with Billing & Invoicing facilitates ‘Peer Review Process’; wherein a SmileExpert Doctor may be asked to reviews the treatment case of another SmileExpert Doctor under Strict confidentiality.
+          </p>
+        </div>
+      </div>
+
+      {/* STEP 4 */}
+      <div className="flex gap-6 items-start">
+        <img
+          src={icon4}
+          alt="icon"
+          className="w-32 h-40 rounded-2xl object-cover"
+        />
+        <div>
+          <p className="text-gray-600 leading-relaxed mt-4">
+            All Doctors and clinic staff undergo mandatory and regular extensive training on new introductions or recent techniques with a view to enhance professional competence in clinical procedures and the infection control practices as laid down by WHO.
+          </p>
+        </div>
+      </div>
+
+      {/* STEP 5 */}
+      <div className="flex gap-6 items-start">
+        <img
+          src={icon5}
+          alt="icon"
+          className="w-32 h-40 rounded-2xl object-cover"
+        />
+        <div>
+          <p className="text-gray-600 leading-relaxed mt-4">
+            The patient record software enables analysis of each case using proprietary methodology developed in-house by a team of Dental Specialists.
+          </p>
+        </div>
+      </div>
+
+      {/* STEP 6 */}
+      <div className="flex gap-6 items-start">
+        <img
+          src={icon6}
+          alt="icon "
+          className="w-32 h-40 rounded-2xl object-cover"
+        />
+        <div>
+          <p className="text-gray-600 leading-relaxed mt-4">
+            SmileExpert Clinic comply to Radiation Safety protocols and radiation safety devices such as lead aprons and thyroid collars are used.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div className="w-full h-[180px] overflow-hidden mt-4">
+  <iframe
+    title="clinic-map"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3445.0390050498395!2d71.9162331!3d30.292951199999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x393b557588a60575%3A0x79afa5462675bee1!2sSmile%20expert%20dental%20clinic!5e0!3m2!1sen!2s!4v1767274320473!5m2!1sen!2s"
+    width="100%"
+    height="450"
+    style={{ border: "0" }}
+    allowFullScreen
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  />
+</div>
+
+<div className="bg-[#0c2340] text-white pt-[60px] pb-[60px] px-[40px] mt-0">
+  <div className="flex justify-between flex-wrap max-w-[1400px] mx-auto gap-[40px]">
+    {/* COLUMN 1 */}
+    <div className="min-w-[200px]">
+      <h3 className="text-[20px] mb-[20px]">Useful Links</h3>
+      <p className="my-[6px]">About Us</p>
+      <p className="my-[6px]">Contact Us</p>
+      <p className="my-[6px]">Meet the Team</p>
+      <p className="my-[6px]">Before & After</p>
+      <p className="my-[6px]">Blogs</p>
+    </div>
+
+    
+    <div className="min-w-[200px]">
+      <h3 className="text-[20px] mb-[20px]">Treatments</h3>
+      <p className="my-[6px]">Exam & X-Rays Check-Up</p>
+      <p className="my-[6px]">Dental Emergency</p>
+      <p className="my-[6px]">Smile Makeover</p>
+      <p className="my-[6px]">Veneers</p>
+      <p className="my-[6px]">Dental Implants</p>
+      <p className="my-[6px]">Root Canal Treatment</p>
+      <p className="my-[6px]">Orthodontic Braces</p>
+      <p className="my-[6px]">Clear Aligners</p>
+    </div>
+
+    <div className="min-w-[200px]">
+      <h3 className="text-[20px] mb-[20px]">Dental Problems</h3>
+      <p className="my-[6px]">Yellow Teeth</p>
+      <p className="my-[6px]">Teeth Cleaning</p>
+      <p className="my-[6px]">Missing Teeth</p>
+      <p className="my-[6px]">Broken Chipped Tooth</p>
+      <p className="my-[6px]">Gum Disease</p>
+      <p className="my-[6px]">Crooked Teeth</p>
+      <p className="my-[6px]">Sensitive & Sore Teeth</p>
+      <p className="my-[6px]">Dental Anxiety</p>
+    </div>
+
+    <div className="min-w-[200px]">
+      <h3 className="text-[20px] mb-[20px]">Patient Safety</h3>
+      <p className="my-[6px]">7X SAFETY</p>
+      <p className="my-[6px]">4 Step Sterilization</p>
+      <p className="my-[6px]">Safety Equipment</p>
+      <p className="my-[6px]">Equipment & Technology</p>
+      <p className="my-[6px]">Quality</p>
+    </div>
+
+    <div className="min-w-[250px]">
+      <h3 className="text-[20px] mb-[20px]">Get in touch</h3>
+
+      <p className="my-2 flex gap-[10px]">✉ aneelabrar555@gmail.com</p>
+
+      <p className="my-2 flex gap-[10px] ">☏ 03008395562</p>
+
+      <p className="my-2 flex gap-[10px] leading-[1.4]">
+        ⚲ Saddique hospital stadium road khanewal
+      </p>
+
+      <p className="mt-6 font-semibold">🕒 Clinic Timing<br/> 11:00am to 9:00pm</p>
+
+      <div className="text-left bg-[#0c2340] text-white py-[50px] px-[20px]">
+        <div className="flex gap-[15px] mt-[20px]">
+          <a
+            href="#"
+            className="w-[40px] h-[40px] bg-[#112d53] flex justify-center items-center rounded-[8px] no-underline"
+          >
+            <FaFacebookF size={20} color="white" />
+          </a>
+
+          <a
+            href="#"
+            className="w-[40px] h-[40px] bg-[#112d53] flex justify-center items-center rounded-[8px] no-underline"
+          >
+            <FaInstagram size={20} color="white" />
+          </a>
+
+          <a
+            href="#"
+            className="w-[40px] h-[40px] bg-[#112d53] flex justify-center items-center rounded-[8px] no-underline"
+          >
+            <FaYoutube size={22} color="white" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+    </>
+  )
+}
