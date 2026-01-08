85ad7e40abcf14ef84984c4e59067570bc3d77ed
abdulmoid648 <abdulmoid648@gmail.com>
2026-01-08 21:26:30 +0000

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  important: true,
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+        jost: ["Jost", "sans-serif"],
+      },
+      colors: {
+        brandDark: "#0f2b48",
+        brandLight: "#d1f3ff",
+        brandAccent: "#33c1ff",
+      },
+    },
+  },
+  // YE WALA BLOCK ADD KAREIN AGAR VERSION 2 HAI
+  variants: {
+    extend: {
+      display: ['group-hover'],
+      visibility: ['group-hover'],
+      opacity: ['group-hover'],
+    },
   },
   plugins: [],
 };
